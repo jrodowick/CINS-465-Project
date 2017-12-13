d1207126082c59576aafdb821a441fd7a2f47918
jrodowick <jrodowick@mail.csuchico.edu>
2017-12-13 12:25:51 -0800

    clearing out files

--- CINS 465 Project/sportsapp/static/js/app.js
+++
@@ -1,6 +0,0 @@
-function get_location() {
-
- var location = document.getElementByID("map_search").value;
- return var;
-
-}
--- CINS 465 Project/sportsapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name = 'index'),
-    url(r'register/$',views.register,name='register'),
-    url(r'event/$',views.sport_event,name='event'),
-    url(r'team/$', views.sports_team, name='team'),
-]
